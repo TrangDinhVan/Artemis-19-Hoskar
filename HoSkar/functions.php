5a808b4005b35d54be622b265838d5eb263037fb
TrangMacMini <trangdv@outlook.com>
2024-05-31 09:15:01 +0700

    Pull from FTP

--- HoSkar/functions.php
+++ HoSkar/functions.php
@@ -67,4 +67,93 @@ function add_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+// Gallery post type
+add_theme_support('post-thumbnails');
+add_post_type_support( 'gallery', 'thumbnail' );    
+function create_posttype_gallery() {
+ 
+    register_post_type( 'gallery',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Gallery' ),
+                'singular_name' => __( 'Gallery' )
+            ),
+            'supports' => array(
+            'title', 
+            'thumbnail',
+            'editor',
+            'custom-fields'
+            ), 
+            'taxonomies' => array( 'category', 'post_tag',  ), 
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'gallery'),
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype_gallery' );
+
+
+function load_more_posts() {
+    $page = isset($_POST['page']) ? intval($_POST['page']) : 1;
+    $category = isset($_POST['category']) ? intval($_POST['category']) : '';
+    $date = isset($_POST['date']) ? sanitize_text_field($_POST['date']) : '';
+
+    $args = array(
+        'post_type' => 'gallery',
+        'posts_per_page' => 12,
+        'paged' => $page,
+    );
+
+    if ($category) {
+        $args['tax_query'] = array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'id',
+                'terms'    => $category,
+            ),
+        );
+    }
+
+    if ($date) {
+        $args['date_query'] = array(
+            array(
+                'after'     => $date,
+                'inclusive' => true,
+            ),
+        );
+    }
+
+    $query = new WP_Query($args);
+
+    $posts_html = '';
+    $no_more_posts = false;
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            $posts_html .= '<a class="gallery-item" href="' . get_the_post_thumbnail_url(get_the_ID(), 'full') . '" target="_blank" data-fancybox="mygallery" data-caption="' . get_the_title() . '" title="' . get_the_title() . '">';
+            $posts_html .= '<img src="' . get_the_post_thumbnail_url(get_the_ID(), 'full') . '" alt="' . get_the_title() . '" />';
+            $posts_html .= '</a>';
+        endwhile;
+        
+        if ($query->post_count < 12):
+            $no_more_posts = true;
+        endif;
+    else :
+        $no_more_posts = true;
+    endif;
+
+    wp_reset_postdata();
+
+    echo json_encode(array(
+        'html' => $posts_html,
+        'no_more_posts' => $no_more_posts,
+    ));
+
+    wp_die();
+}
+add_action('wp_ajax_load_more_posts', 'load_more_posts');
+add_action('wp_ajax_nopriv_load_more_posts', 'load_more_posts');
 ?>
